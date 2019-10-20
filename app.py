85117ad28971440edd9da19d23fa2baebaaaa1f4
Flatiron School <flatironschool@Cory-Mac.fios-router.home>
2019-10-19 21:58:01 -0400

    Add camera upload image.

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from PIL import Image
 from tensorflow import keras
 from coffee_prediction import make_prediction
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+                        'https://dash.plot.ly/assets/override.css?m=1571412266.0',
+                        ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server # the Flask app
@@ -20,25 +22,34 @@ model = keras.models.load_model('coffee.h5')
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
+        #children=html.Div([
+            #'Drag and Drop or ',
+        #    html.A('Take a Picture!')
+        #]),
         style={
-            'width': '100%',
+            #'width': '100%',
             'height': '60px',
             'lineHeight': '60px',
             'borderWidth': '1px',
-            'borderStyle': 'dashed',
+            #'borderStyle': 'dashed',
             'borderRadius': '5px',
             'textAlign': 'center',
-            'margin': '10px'
+            'margin': '10px',
+            'background-image': 'url(https://png2.cleanpng.com/sh/86c39b36cbc59bde176d8f6d2f986a53/L0KzQYm3UsA2N6F2iZH0aYP2gLBuTfNidZZ3eZ9yY3BxPcX5gf50eJJ3fdD9LXPkfbb5gb1qa5DzReJ3Zz24cYiCUPQzaZY1S9Q6Mj60QYi7V8IyQGI6SakDOEK5RoeCUcg2NqFzf3==/kisspng-camera-icon-transparent-camera-icon-png-5a790d2ae03b12.1174721815178826669185.png)',
+            'background-repeat': 'no-repeat',
+            'background-position': 'center',
+            'background-size': 'contain',
+            #'background-attachment': 'fixed',
+
         },
         # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-data-upload'),
-])
+
+    html.Div(id='output-data-upload', ),# className='content-container'),
+],) #style={'background-image': 'url(https://i.imgur.com/oOfouhD.jpg)',
+   #       'background-repeat': 'no-repeat',
+   #       'background-position': 'center'} ) #className='background')
 
 
 def parse_contents(contents, filename, date):
@@ -87,4 +98,4 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
