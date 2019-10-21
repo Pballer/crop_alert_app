b98ff17b65a4349215d4711d2c70796a23fc1858
Flatiron School <flatironschool@Cory-Mac.local>
2019-10-21 10:01:09 -0400

    Updated content layout. Added header menu.

--- app.py
+++ app.py
@@ -10,47 +10,74 @@ from PIL import Image
 from tensorflow import keras
 from coffee_prediction import make_prediction
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+external_stylesheets = [#'https://codepen.io/chriddyp/pen/bWLwgP.css',
+                        'static/code_pen.css',
                         'https://dash.plot.ly/assets/override.css?m=1571412266.0',
+                        #'static/dash_override.css'
                         ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Crop Alert!'
 server = app.server # the Flask app
 
 model = keras.models.load_model('coffee.h5')
 
 app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        #children=html.Div([
-            #'Drag and Drop or ',
-        #    html.A('Take a Picture!')
-        #]),
-        style={
-            #'width': '100%',
-            'height': '120px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            #'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px',
-            'background-image': 'url(https://png2.cleanpng.com/sh/86c39b36cbc59bde176d8f6d2f986a53/L0KzQYm3UsA2N6F2iZH0aYP2gLBuTfNidZZ3eZ9yY3BxPcX5gf50eJJ3fdD9LXPkfbb5gb1qa5DzReJ3Zz24cYiCUPQzaZY1S9Q6Mj60QYi7V8IyQGI6SakDOEK5RoeCUcg2NqFzf3==/kisspng-camera-icon-transparent-camera-icon-png-5a790d2ae03b12.1174721815178826669185.png)',
-            'background-repeat': 'no-repeat',
-            'background-position': 'center',
-            'background-size': 'contain',
-            #'background-attachment': 'fixed',
+    html.Div([
+        html.Div([
+            html.Img(src='static/coffee_bean_icon.png', className='logo'),
+            html.Div([
+                html.A('About this site',
+                       target='_blank',
+                       href='https://github.com/Pballer/crop_alert_app',
+                       className='link'),
+            ], className='links'),
+        ], className='container-width', style={'height': '100%'}),
+    ], className='header'),
+    html.Div([
+        html.H1('Upload a picture of your coffee plant!'),
+        dcc.Upload(
+            id='upload-data',
+            #className='content-container container',
+            #children=html.Div([
+                #'Drag and Drop or ',
+            #    html.A('Take a Picture!')
+            #]),
+            style={
+                #'width': '100%',
+                'height': '120px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'color': 'green',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+                'background-image': 'url(static/camera_icon.png)', #'url(https://png2.cleanpng.com/sh/86c39b36cbc59bde176d8f6d2f986a53/L0KzQYm3UsA2N6F2iZH0aYP2gLBuTfNidZZ3eZ9yY3BxPcX5gf50eJJ3fdD9LXPkfbb5gb1qa5DzReJ3Zz24cYiCUPQzaZY1S9Q6Mj60QYi7V8IyQGI6SakDOEK5RoeCUcg2NqFzf3==/kisspng-camera-icon-transparent-camera-icon-png-5a790d2ae03b12.1174721815178826669185.png)',
+                'background-repeat': 'no-repeat',
+                'background-position': 'center',
+                'background-size': 'contain',
+                #'background-attachment': 'fixed',
 
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+        ),
 
-    html.Div(id='output-data-upload', className='content-container container'),
-], className='background') #style={'background-image': 'url(https://i.imgur.com/oOfouhD.jpg)',
+        html.Div(id='output-data-upload', ), #className='container'),
+    ], className='content-container container-width content', id='chapter'),
+], className='background') #, style={'background-image': 'url(https://i.imgur.com/oOfouhD.jpg)'})
    #       'background-repeat': 'no-repeat',
    #       'background-position': 'center'} ) #className='background')
 
+disease_info = {
+    'Rust' : html.A('What is Rust?', target='_blank',
+               href='https://www.apsnet.org/edcenter/disandpath/fungalbasidio/pdlessons/Pages/CoffeeRust.aspx',
+               className='link'),
+    'Red Spider Mite' : html.A('What are Red Spider Mites?', target='_blank',
+               href='http://entnemdept.ufl.edu/creatures/orn/shrubs/southern_red_mite.htm',
+               className='link'),
+}
 
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
@@ -84,8 +111,11 @@ def parse_contents(contents, filename, date):
                     #'padding-top' : 0,
                     #'padding-right' : 0
                 }),
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')),
+        html.P(),
+        disease_info.get(prediction, html.Div()),
+        html.Hr(),
+        html.P(filename),
+        html.P(datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')),
         #html.Hr(),
     ], className='example-container')
 
